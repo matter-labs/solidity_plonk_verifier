9ce43aadf6a6a1027b1943676c413afe5448b059
Sait Imamoglu <nuggez@gmail.com>
2021-07-28 03:59:45 +0300

    update codegen and template

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use handlebars::*;
 // use serde_json::{Map, Serializer};
-use franklin_crypto::bellman::{CurveAffine, Engine, PrimeField, PrimeFieldRepr, pairing::bn256::{Bn256, Fq, Fr}, plonk::{better_better_cs::{cs::Circuit, setup::VerificationKey}, domains::Domain}};
+use franklin_crypto::bellman::{CurveAffine, Engine, PrimeField, PrimeFieldRepr, pairing::bn256::{Bn256, Fq, Fr, Fq2}, plonk::{better_better_cs::{cs::Circuit, setup::VerificationKey}, domains::Domain}};
 use serde_json::Map;
 #[macro_use]
 extern crate serde_derive;
@@ -8,11 +8,32 @@ extern crate serde_json;
 
 use serde::ser::{Serialize, SerializeStruct, Serializer};
 
-pub struct Commitment<E: Engine> {
-    x: E::Fq,
-    y: E::Fq,
+
+
+pub struct FieldElement(Fr);
+
+
+impl From<Fr>  for FieldElement {
+    fn from(el: Fr) -> Self {
+        Self(el)
+    }
 }
-pub struct FieldElement<F: PrimeField>(F);
+
+impl Serialize for FieldElement {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut state = serializer.serialize_struct("FieldElement", 1)?;
+        
+        let mut init_str = String::from("PairingsBn254.new_fr");
+        init_str.push_str("(");
+        init_str.push_str(&field_element_to_hex(&self.0));
+        init_str.push_str(")");
+        state.serialize_field("el", &init_str)?;        
+        state.end()
+    }
+}
 
 fn field_element_to_hex<F: PrimeField>(el: &F) -> String {
     let mut buf = [0u8; 32];
@@ -27,26 +48,24 @@ fn field_element_to_hex<F: PrimeField>(el: &F) -> String {
 }
 
 
-impl<E: Engine> Commitment<E> {
-    fn from_affine_point(point: E::G1Affine) -> Self {
+pub struct G1Point {
+    x: Fq,
+    y: Fq,
+}
+
+impl G1Point {
+    fn from_affine_point(point: <Bn256 as Engine>::G1Affine) -> Self {
         let (x, y) = point.into_xy_unchecked();
         Self { x, y }
     }
 }
 
-impl<F: PrimeField> From<F>  for FieldElement<F> {
-    fn from(el: F) -> Self {
-        Self(el)
-    }
-}
-
-impl<E: Engine> Serialize for Commitment<E> {
+impl Serialize for G1Point {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
-        // 3 is the number of fields in the struct.
-        let mut state = serializer.serialize_struct("Commitment", 2)?;
+        let mut state = serializer.serialize_struct("G1Point", 2)?;
 
         let mut init_str = String::from("PairingsBn254.new_g1");
         init_str.push_str("(");
@@ -58,23 +77,42 @@ impl<E: Engine> Serialize for Commitment<E> {
         state.end()
     }
 }
-impl<F: PrimeField> Serialize for FieldElement<F> {
+
+pub struct G2Point {
+    x: Fq2,
+    y: Fq2,
+}
+
+impl G2Point {
+    fn from_affine_point(point: <Bn256 as Engine>::G2Affine) -> Self {
+        let (x, y) = point.into_xy_unchecked();
+        Self { x, y }
+    }
+}
+
+impl Serialize for G2Point {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
         // 3 is the number of fields in the struct.
-        let mut state = serializer.serialize_struct("FieldElement", 1)?;
-        
-        let mut init_str = String::from("PairingsBn254.new_fr");
-        init_str.push_str("(");
-        init_str.push_str(&field_element_to_hex(&self.0));
-        init_str.push_str(")");
-        state.serialize_field("fr", &init_str)?;
-        // state.serialize_field("el", &field_element_to_hex(&self.0))?;
+        let mut state = serializer.serialize_struct("G2Point", 2)?;
+        let mut init_str = String::from("PairingsBn254.new_g2");
+
+        init_str.push_str("([");
+        init_str.push_str(&field_element_to_hex(&self.x.c0));
+        init_str.push_str(",");
+        init_str.push_str(&field_element_to_hex(&self.x.c1));
+        init_str.push_str("],[");
+        init_str.push_str(&field_element_to_hex(&self.y.c0));
+        init_str.push_str(",");
+        init_str.push_str(&field_element_to_hex(&self.y.c1));
+        init_str.push_str("])");
+        state.serialize_field("g2", &init_str)?;
         state.end()
     }
 }
+
 
 struct MapWrapper{
     inner: Map<String, JsonValue>,
@@ -91,7 +129,7 @@ impl MapWrapper{
     }
 }
 
-pub fn render_verification_key<E: Engine, C: Circuit<E>>(vk: &VerificationKey<E, C>, render_to_path: &str) {
+pub fn render_verification_key<C: Circuit<Bn256>>(vk: &VerificationKey<Bn256, C>, render_to_path: &str) {
 
     let mut map = MapWrapper::new();
     let mut handlebars = Handlebars::new();
@@ -99,23 +137,65 @@ pub fn render_verification_key<E: Engine, C: Circuit<E>>(vk: &VerificationKey<E,
     // global constants
 
     // main gate + custom rescue
-    map.insert("NUM_GATES", 2); 
+    map.insert("NUM_GATES", vk.gate_selectors_commitments.len()); 
      // qL, qR, qM, qO, qConst, qD, qDnext
-    map.insert("NUM_SELECTORS", 7);
+    map.insert("NUM_SELECTORS", vk.gate_setup_commitments.len());
     // a, b, c, d
-    map.insert("STATE_WIDTH", 4); 
+    map.insert("STATE_WIDTH", vk.state_width); 
+    map.insert("DNEXT_INDEX", vk.state_width-1); 
+    map.insert("NUM_G2_ELS", vk.g2_elements.len());     
+    map.insert("NUM_LOOKUP_TABLES", vk.lookup_tables_commitments.len()); 
+    map.insert("SERIALIZED_PROOF_LENGTH", 0); 
 
-    let domain: Domain<E::Fr> = Domain::new_for_size(vk.n as u64).expect("a domain");
+    // domain
+    let domain: Domain<Fr> = Domain::new_for_size(vk.n as u64).expect("a domain");
     map.insert("domain_size".into(), domain.size);
     map.insert("domain_generator".into(), FieldElement::from(domain.generator));
 
-    let mut setup_commitments = vec![];
-
+    // G1Points
+    let mut gate_setup_commitments = vec![];
     for cmt in vk.gate_setup_commitments.iter() {
-        setup_commitments.push(Commitment::<E>::from_affine_point(cmt.clone()))
+        gate_setup_commitments.push(G1Point::from_affine_point(cmt.clone()))
     }
+    map.insert("gate_setup_commitments", gate_setup_commitments);
 
-    map.insert("setup_commitments", setup_commitments);
+    let mut gate_selectors_commitments = vec![];
+    for cmt in vk.gate_selectors_commitments.iter() {
+        gate_selectors_commitments.push(G1Point::from_affine_point(cmt.clone()))
+    }
+    map.insert("gate_selectors_commitments", gate_selectors_commitments);
+    
+    let mut permutation_commitments = vec![];
+    for cmt in vk.permutation_commitments.iter() {
+        permutation_commitments.push(G1Point::from_affine_point(cmt.clone()))
+    }
+    map.insert("permutation_commitments", permutation_commitments);    
+    
+    map.insert("total_lookup_entries_length", vk.total_lookup_entries_length);    
+    map.insert("lookup_selector_commitment", G1Point::from_affine_point(vk.lookup_selector_commitment.unwrap_or(<Bn256 as Engine>::G1Affine::zero())));
+    if vk.total_lookup_entries_length > 0 {
+        assert!(vk.lookup_selector_commitment.is_some());
+    }
+    let mut lookup_tables_commitments = vec![];
+    for cmt in vk.lookup_tables_commitments.iter() {
+        lookup_tables_commitments.push(G1Point::from_affine_point(cmt.clone()))
+    }
+    map.insert("lookup_tables_commitments", lookup_tables_commitments);
+    map.insert("lookup_table_type_commitment", G1Point::from_affine_point(vk.lookup_table_type_commitment.unwrap_or(<Bn256 as Engine>::G1Affine::zero())));
+    
+    // non residues
+    let mut non_residues = vec![];
+    for el in vk.non_residues.iter() {
+        non_residues.push(FieldElement::from(el.clone()));
+    }
+    map.insert("non_residues", non_residues);
+    
+    // pairing g2 elements
+    let mut g2_elements = vec![];
+    for point in vk.g2_elements.iter() {
+        g2_elements.push(G2Point::from_affine_point(point.clone()));
+    }
+    map.insert("g2_elements", g2_elements);
 
     // register template from a file and assign a name to it
     handlebars
@@ -156,6 +236,6 @@ mod tests {
         );
 
         let vk = VerificationKey::<Bn256, TestCircuit>::read(&mut reader).expect("parsed vk");
-        render_verification_key(&vk, "./contracts/Verifier.sol");
+        render_verification_key(&vk, "./hardhat/contracts/VerificationKey.sol");
     }
 }
--- template/verifier.sol
+++ template/verifier.sol
@@ -1,38 +1,229 @@
 pragma solidity >0.7;
 
 import "./Pairing.sol";
+import "./Verifier.sol";
 
 uint256 constant STATE_WIDTH = {{STATE_WIDTH}};
 uint256 constant NUM_SELECTORS = {{NUM_SELECTORS}};
 uint256 constant NUM_GATES = {{NUM_GATES}};
+uint256 constant NUM_G2_ELS = {{NUM_G2_ELS}};
+uint256 constant NUM_LOOKUP_TABLES = {{NUM_LOOKUP_TABLES}};
+uint256 constant SERIALIZED_PROOF_LENGTH = {{SERIALIZED_PROOF_LENGTH}};
 
 struct VerificationKey {
     uint256 domain_size;
     uint256 num_inputs;
     PairingsBn254.Fr omega;
-    PairingsBn254.G1Point[NUM_GATES] gate_selector_commitments;
+    PairingsBn254.G1Point[NUM_GATES] gate_selectors_commitments;
     PairingsBn254.G1Point[NUM_SELECTORS] gate_setup_commitments; // STATE_WIDTH for witness + multiplication + constant
     PairingsBn254.G1Point[STATE_WIDTH] permutation_commitments;
-    // PairingsBn254.G1Point[ACCESSIBLE_STATE_POLYS_ON_NEXT_STEP] next_step_gate_setup_commitments;
     uint256 total_lookup_entries_length;
-    PairingsBn254.G1Point lookup_gate_setup_commitment;
+    PairingsBn254.G1Point lookup_selector_commitment;
+    PairingsBn254.G1Point[NUM_LOOKUP_TABLES] lookup_tables_commitments;
     PairingsBn254.G1Point lookup_table_type_commitment;
-    PairingsBn254.G1Point[] lookup_tables_commitments;
 
-    PairingsBn254.Fr[STATE_WIDTH-1] permutation_non_residues;
-    PairingsBn254.G2Point g2_x;
+    PairingsBn254.Fr[STATE_WIDTH-1] non_residues;
+    PairingsBn254.G2Point[NUM_G2_ELS] g2_elements;
 }
 
-contract Verifier{
+contract Verifier is Plonk4VerifierWithAccessToDNext{
 
-    function get_vk_key() internal pure returns(VerificationKey memory vk){
+    function get_verification_key() internal pure returns(VerificationKey memory vk){
         vk.domain_size = {{domain_size}};
-        vk.omega = {{domain_generator.fr}};
-        {{#each setup_commitments}}
+        vk.omega = {{domain_generator.el}};
+        // coefficients
+        {{#each gate_setup_commitments}}
         vk.gate_setup_commitments[{{@index}}] = {{this.g1}};
         {{/each}}
-
-
+        // gate selectors
+        {{#each gate_selectors_commitments}}
+        vk.gate_selectors_commitments[{{@index}}] = {{this.g1}};
+        {{/each}}
+        // permutation
+        {{#each permutation_commitments}}
+        vk.permutation_commitments[{{@index}}] = {{this.g1}};
+        {{/each}}
+        vk.total_lookup_entries_length = {{total_lookup_entries_length}};
+        
+        // lookup table commitments
+        vk.total_lookup_entries_length = {{total_lookup_entries_length}};
+        vk.lookup_selector_commitment = {{lookup_selector_commitment.g1}};
+        {{#each lookup_tables_commitments}}
+        vk.lookup_tables_commitments[{{@index}}] = {{this.g1}};
+        {{/each}}
+        vk.lookup_table_type_commitment = {{lookup_table_type_commitment.g1}};
+        
+        // non residues
+        {{#each non_residues}}
+        vk.non_residues[{{@index}}] = {{this.el}};
+        {{/each}}
+        
+        // g2 elements
+        {{#each g2_elements}}
+        vk.g2_elements[{{@index}}] = {{this.g2}};
+        {{/each}}
     }
 
+
+    function deserialize_proof(
+        uint256[] memory public_inputs, 
+        uint256[] memory serialized_proof
+    ) internal pure returns(Proof memory proof) {
+        require(serialized_proof.length == SERIALIZED_PROOF_LENGTH);
+        proof.input_values = new uint256[](public_inputs.length);
+        for (uint256 i = 0; i < public_inputs.length; i++) {
+            proof.input_values[i] = public_inputs[i];
+        }
+ 
+        uint256 j = 0;
+        for (uint256 i = 0; i < STATE_WIDTH; i++) {
+            proof.state_polys_commitments[i] = PairingsBn254.new_g1_checked(
+                serialized_proof[j],
+                serialized_proof[j+1]
+            );
+
+            j += 2;
+        }
+        
+        proof.copy_permutation_grand_product_commitment = PairingsBn254.new_g1_checked(
+                serialized_proof[j],
+                serialized_proof[j+1]
+        );
+        j += 2;
+        
+        proof.lookup_s_poly_commitment = PairingsBn254.new_g1_checked(
+                serialized_proof[j],
+                serialized_proof[j+1]
+        );
+        j += 2;
+        
+        proof.lookup_grand_product_commitment = PairingsBn254.new_g1_checked(
+                serialized_proof[j],
+                serialized_proof[j+1]
+        );
+        j += 2;
+        
+        for (uint256 i = 0; i < STATE_WIDTH; i++) {
+            proof.quotient_poly_parts_commitments[i] = PairingsBn254.new_g1_checked(
+                serialized_proof[j],
+                serialized_proof[j+1]
+            );
+
+            j += 2;
+        }
+        
+        for (uint256 i = 0; i < STATE_WIDTH; i++) {
+            proof.state_polys_openings_at_z[i] = PairingsBn254.new_fr(
+                serialized_proof[j]
+            );
+
+            j += 1;
+        }
+        
+        for (uint256 i = 0; i < proof.state_polys_openings_at_z_omega.length; i++) {
+            proof.state_polys_openings_at_z_omega[i] = PairingsBn254.new_fr(
+                serialized_proof[j]
+            );
+
+            j += 1;
+        }
+
+        for (uint256 i = 0; i < proof.gate_setup_openings_at_z.length; i++) {
+            proof.gate_setup_openings_at_z[i] = PairingsBn254.new_fr(
+                serialized_proof[j]
+            );
+
+            j += 1;
+        }
+        for (uint256 i = 0; i < proof.gate_selectors_openings_at_z.length; i++) {
+            proof.gate_selectors_openings_at_z[i] = PairingsBn254.new_fr(
+                serialized_proof[j]
+            );
+
+            j += 1;
+        }
+        for (uint256 i = 0; i < proof.copy_permutation_polys_openings_at_z.length; i++) {
+            proof.copy_permutation_polys_openings_at_z[i] = PairingsBn254.new_fr(
+                serialized_proof[j]
+            );
+
+            j += 1;
+        }
+        
+        proof.copy_permutation_grand_product_opening_at_z_omega = PairingsBn254.new_fr(
+                serialized_proof[j]
+            );
+
+        j += 1;
+
+        proof.lookup_s_poly_opening_at_z_omega = PairingsBn254.new_fr(
+                serialized_proof[j]
+            );
+
+        j += 1;
+        proof.lookup_grand_product_opening_at_z_omega = PairingsBn254.new_fr(
+                serialized_proof[j]
+            );
+
+        j += 1;
+
+        proof.lookup_t_poly_opening_at_z = PairingsBn254.new_fr(
+                serialized_proof[j]
+            );
+
+        j += 1;
+
+        proof.lookup_t_poly_opening_at_z_omega = PairingsBn254.new_fr(
+                serialized_proof[j]
+            );
+
+        j += 1;
+        proof.lookup_selector_poly_opening_at_z = PairingsBn254.new_fr(
+                serialized_proof[j]
+            );
+
+        j += 1;
+        proof.lookup_table_type_poly_opening_at_z = PairingsBn254.new_fr(
+                serialized_proof[j]
+            );
+
+        j += 1;
+
+        proof.quotient_poly_opening_at_z = PairingsBn254.new_fr(
+            serialized_proof[j]
+        );
+
+        j += 1;
+
+        proof.linearization_poly_opening_at_z = PairingsBn254.new_fr(
+            serialized_proof[j]
+        );
+
+        j += 1;
+
+        proof.opening_proof_at_z = PairingsBn254.new_g1_checked(
+                serialized_proof[j],
+                serialized_proof[j+1]
+        );
+        j += 2;
+
+        proof.opening_proof_at_z_omega = PairingsBn254.new_g1_checked(
+                serialized_proof[j],
+                serialized_proof[j+1]
+        );
+    }
+    
+    function verify_serialized_proof(
+        uint256[] memory public_inputs, 
+        uint256[] memory serialized_proof
+    ) public view returns (bool) {
+        VerificationKey memory vk = get_verification_key();
+        require(vk.num_inputs == public_inputs.length);
+
+        Proof memory proof = deserialize_proof(public_inputs, serialized_proof);
+
+        bool valid = verify(proof, vk);
+
+        return valid;
+    }  
 }
